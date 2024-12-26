5674a093650de44538a43ae4072d1b897e01ebf5
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2024-12-26 14:57:45 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://mangostore-backend.onrender.com",
-      "/uploads/": "https://mangostore-backend.onrender.com",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
