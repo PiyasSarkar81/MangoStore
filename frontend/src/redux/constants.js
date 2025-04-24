bdd36683fb65d1c349c6f3dd12cfec486069fddd
Piyas <Piyas@piyassarkar81@gmail.com>
2025-04-24 23:30:11 +0530

    change in front endconstant.js file

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,15 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = `${import.meta.env.VITE_API_URL}`;
+export const USERS_URL = `${import.meta.env.VITE_API_URL}/api/users`;
+export const CATEGORY_URL = `${import.meta.env.VITE_API_URL}/api/category`;
+export const PRODUCT_URL = `${import.meta.env.VITE_API_URL}/api/products`;
+export const UPLOAD_URL = `${import.meta.env.VITE_API_URL}/api/upload`;
+export const ORDERS_URL = `${import.meta.env.VITE_API_URL}/api/orders`;
+export const PAYPAL_URL = `${import.meta.env.VITE_API_URL}/api/config/paypal`;
+
+// export const BASE_URL = "";
+// export const USERS_URL = "/api/users";
+// export const CATEGORY_URL = "/api/category";
+// export const PRODUCT_URL = "/api/products";
+// export const UPLOAD_URL = "/api/upload";
+// export const ORDERS_URL = "/api/orders";
+// export const PAYPAL_URL = "/api/config/paypal";
