77a6d116610d05ea62719bbb7b3aa9c456d4224a
Piyas <Piyas@piyassarkar81@gmail.com>
2025-04-24 21:58:05 +0530

    changes in url in backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const url = process.env.MONGO_URI || "https://mangostore-c12m.onrender.com"
-
-const connectDB = async () => {
+const url = "https://mangostore-c12m.onrender.com"
+// const url = process.env.MONGO_URI 
+const connectDB = async (url) => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect();
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
