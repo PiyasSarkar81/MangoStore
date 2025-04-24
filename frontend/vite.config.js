1ee5b74df992e97d097a1aa3f2d2ae4813e7dd38
Piyas <Piyas@piyassarkar81@gmail.com>
2025-04-24 23:31:49 +0530

    change in front endconstant.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Check if running locally or in production
-// eslint-disable-next-line no-undef
-const API_URL = process.env.NODE_ENV === "production"
-  ? "https://mangostore-c12m.onrender.com"  // Replace with your actual Render backend URL
-  : "http://localhost:5000";
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": API_URL,
-      "/uploads/": API_URL,
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
-// https://vitejs.dev/config/
