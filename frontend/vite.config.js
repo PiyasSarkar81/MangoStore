20a9ede07ff9fab85769f7753528a2d3b3ebc170
Piyas <Piyas@piyassarkar81@gmail.com>
2025-05-24 13:58:05 +0530

    change local host to global server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://mangostore-c12m.onrender.com",
+      "/uploads/": "https://mangostore-c12m.onrender.com",
     },
   },
 });
